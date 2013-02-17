938e82713036494bd7b37b1fc95680112ae3908c
Rupert <rupert@2rmobile.com>
2013-02-17 18:42:11 +1100

    Removing unnecessary import of UISettings

--- MyFirstMap/src/com/example/MyFirstMap/MapActivity.java
+++ MyFirstMap/src/com/example/MyFirstMap/MapActivity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
